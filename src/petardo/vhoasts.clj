4a96586a0db16d7f92e01764221ed96f663e4ad0
Rune Juhl Jacobsen <runejuhl@petardo.dk>
2020-04-16 00:33:01 +0200

    Add functions to fetch data, find matches for multiple domains

--- src/petardo/vhoasts.clj
+++ src/petardo/vhoasts.clj
@@ -27,12 +27,33 @@
     (map (partial map crude-html-strip*))
     (map vec)
     (map (fn [entry] (update entry 4 (fn [s] (clojure.string/split s #"<BR>")))))
-    (map (partial zipmap [:crt.sh-id :logged-at :not-before :not-after :match :issuer]))
-    ))
+    (map (partial zipmap [:crt.sh-id :logged-at :not-before :not-after :match :issuer]))))
 
-(defn unique-domains
+(defn unique-matches
+  "Get all unique matches from a set of results."
   [ds]
   (->> ds
     (map :match)
     (apply concat)
     set))
+
+(defn fetch-crt.sh
+  "Fetch data from crt.sh for a given pattern `pattern`."
+  [pattern]
+  (let [{:keys [status body] :as response} (curl/get
+                                             "https://crt.sh/"
+                                             {:query-params {"q" pattern}})]
+    (when (= status 200)
+      body)))
+
+(defn find-unique-matches
+  "Given a set of domain patterns `domains` return a complete set of unique
+  matches."
+  [& domains]
+  (->> domains
+    (map #(some->> %
+            (fetch-crt.sh)
+            (raw->hash-map)
+            (unique-matches)))
+    (apply concat)
+    (set)))
